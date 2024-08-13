ddd72c6c0b5392b01a847fdbbdbf2434d46894bb
francogaray <francogaray.dev@gmail.com>
2024-08-13 00:30:59 -0300

    first commit

---
+++ algoritmos.js
@@ -0,0 +1,9 @@
+// 1. Crear un algoritmo que muestre los números impares entre el 0 y el 100.
+
+//1. Crear un algoritmo que muestre los números impares entre el 0 y el 100.
+
+//2. Realizar un programa que ingrese los sueldos de 5 operarios en un vector. Realizar la creación y carga del vector en el constructor. Crear un método para imprimir el vector.
+
+//3. Plantear una clase llamada Alumno y definir como atributos su nombre y su edad. En el constructor realizar el ingreso de datos. Definir otros dos métodos para imprimir los datos ingresados y un mensaje si es mayor o no de edad (edad >= 18)
+
+// 4. JavaScript ES6: Dados los siguientes array, imprimir por consola los elementos del array “y” que no se encuentran en el array “x” utilizando para tal fin una única línea de código. const x = ["n", "bro", "c", "|"]; const y = ["d", "n", "l", "bro", "g"];
